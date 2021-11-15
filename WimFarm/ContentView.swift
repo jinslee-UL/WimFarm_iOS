4a109e790322433b0965aa63423713464a564440
jinslee <jinslee@ulalalab.com>
2021-11-15 12:00:19 +0900

    Initial Commit

---
+++ WimFarm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WimFarm
+//
+//  Created by ulalalab on 2021/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WimFarm/WimFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  WimFarmApp.swift
+//  WimFarm
+//
+//  Created by ulalalab on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct WimFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
